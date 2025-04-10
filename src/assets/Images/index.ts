4caff1eb26666953f2ae889d5aac667b7401a805
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-04-10 00:41:02 -0300

    chore: added icon

--- src/assets/Images/index.ts
+++ src/assets/Images/index.ts
@@ -1,3 +1,4 @@
 export { default as BackgroundImage } from './Background-image.svg';
 export { default as BackgroundCircle } from './background-circle.svg';
 export { default as HeroImage } from './main-image-car.svg';
+export { default as IconSecurity } from './icon-security.svg';
