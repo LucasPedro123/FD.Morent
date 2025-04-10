f26e94a9206c1d61d562aebd75ba9a2442254fa4
Lucas Fernandes <lucaspedrofernandes@gmail.com>
2025-04-10 00:41:42 -0300

    feat: added 'payment' and 'car-details' pages to routes

--- src/route.tsx
+++ src/route.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import { Home } from "./pages";
+import { CarDetails, Home, Payment } from "./pages";
 
 export const Router: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/:id" element={<Home />} />
+      <Route path="/car-details/:id" element={<CarDetails />} />
+      <Route path="/payment" element={<Payment />} />
     </Routes>
   );
 };
